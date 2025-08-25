ff6c8fe03952926ea723e68be0f70e4aaed7eeae
pawi1 <p.rutkow@o2.pl>
2025-08-25 16:36:35 +0200

    Delete IDE Files

--- Debug/src/main.d
+++
@@ -1,2 +0,0 @@
-src/main.o: ../src/main.c ../src/strings_utils.h
-../src/strings_utils.h:
--- Debug/src/strings_utils.d
+++
@@ -1,2 +0,0 @@
-src/strings_utils.o: ../src/strings_utils.c ../src/strings_utils.h
-../src/strings_utils.h:
